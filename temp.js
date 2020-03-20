ca41c9ddd58d78b09332e99d7fb27f52bff26456
Your Name <you@example.com>
2020-03-20 08:49:12 +0800

    temp

---
+++ temp.js
@@ -0,0 +1,13 @@
+(function(){
+    function reduce(array, combine, start) {
+        var current = start;
+        for (var element of array) {
+            current = combine(current, element);
+        }
+        return current;
+        }
+        var max=function(data){
+            return reduce(data,(a,b)=>a>b.credit?a:b.credit)
+        }
+        window.my={max:max}
+    })()
